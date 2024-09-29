a6d6f0b9c4b995b37dbfae964ee1bd42cb081d01
Lucas Souza <lucas.souza.andrade66@gmail.com>
2024-09-28 23:16:19 -0300

    Adição do modelo de teste

--- models_api.py
+++ models_api.py
@@ -1,6 +1,15 @@
+from sklearn.linear_model import LinearRegression
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Dict
+import pandas as pd
+import joblib
+import os
+
+current_dir = os.path.dirname(__file__)
+model_path = os.path.join(current_dir, 'Models', 'model_lr_semnormalizar.pkl')
+
+loaded_model = joblib.load(model_path)
 
 app = FastAPI()
 
@@ -27,5 +36,12 @@ class StudentData(BaseModel):
 
 @app.post("/")
 async def model_predict(data: StudentData) -> Dict[str, int]:
-    result = {key: value * 2 for key, value in data.model_dump().items()}
-    return result
+    df = pd.DataFrame([data.model_dump()])
+    df = df[['Hours_Studied', 'Attendance', 'Parental_Involvement',
+       'Access_to_Resources', 'Extracurricular_Activities', 'Sleep_Hours',
+       'Previous_Scores', 'Motivation_Level', 'Internet_Access',
+       'Tutoring_Sessions', 'Family_Income', 'Teacher_Quality', 'School_Type',
+       'Peer_Influence', 'Physical_Activity', 'Learning_Disabilities',
+       'Parental_Education_Level', 'Distance_from_Home', 'Gender']]
+    result = loaded_model.predict(df)
+    return {"prediction": round(result[0], 0)}
